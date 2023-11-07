05327c62fd7e943a3876fb4a3fc15db7ab6c5c8b
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-11-06 21:12:29 -0300

    Present project

--- trabalho2/pesquisa_grupo/views.py
+++ trabalho2/pesquisa_grupo/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from .models import Integrante, Publicacao
 
 def index(request):
     integrantes = Integrante.objects.all()
-    return render(request, 'pesquisa_grupo/index.html', {'integrantes': integrantes})
+    tipo_count = Integrante.objects.values('tipo').annotate(total=Count('tipo'))
+    return render(request, 'pesquisa_grupo/index.html', {
+        'integrantes': integrantes,
+        'tipo_count': tipo_count
+    })
 
 def detalhes_integrante(request, integrante_id):
     integrante = get_object_or_404(Integrante, pk=integrante_id)
