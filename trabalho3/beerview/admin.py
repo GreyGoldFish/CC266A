f75d9a22553993bd77b19a8067ac907259e668e1
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-11-08 13:39:55 -0300

    Modificar admin.py de beerview

--- trabalho3/beerview/admin.py
+++ trabalho3/beerview/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import TipoEstilo, EstiloCerveja, Cerveja, Avaliacao, Endereco, Cervejaria
 
-# Register your models here.
+class AvaliacaoInline(admin.TabularInline):
+    model = Avaliacao
+    extra = 1  # Quantas linhas para adicionar avaliações serão mostradas
+
+class CervejaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cervejaria', 'estilo', 'vol')  # Campos que aparecem na listagem
+    list_filter = ('estilo', 'cervejaria')  # Filtros disponíveis na barra lateral
+    search_fields = ('nome', 'cervejaria__nome')  # Campos pesquisáveis
+    inlines = [AvaliacaoInline]
+
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('cerveja', 'usuario', 'nota', 'comentario')
+    list_filter = ('nota', 'usuario')
+    search_fields = ('cerveja__nome', 'usuario__username')
+
+admin.site.register(TipoEstilo)
+admin.site.register(EstiloCerveja)
+admin.site.register(Cerveja, CervejaAdmin)
+admin.site.register(Avaliacao, AvaliacaoAdmin)
+admin.site.register(Endereco)
+admin.site.register(Cervejaria)
