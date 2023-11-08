86aeb901204e4531c364f9cd3d65533377ea6b92
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-11-08 13:34:56 -0300

    Atualizar modelo de endereco

--- trabalho3/beerview/models.py
+++ trabalho3/beerview/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django_countries.fields import CountryField
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
 from decimal import Decimal
 
 
